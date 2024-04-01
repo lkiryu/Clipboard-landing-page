773f51817620d4916833a13d672f3acc24af8e3f
Kiryu <13matheusilva@gmail.com>
2024-04-01 17:18:24 -0300

    section access style add

--- src/components/sectionAccess/sectionAccess.js
+++ src/components/sectionAccess/sectionAccess.js
@@ -1,10 +1,12 @@
 import React from "react";
 import imageDevices from '../../images/image-devices.png'
 
+import './sectionAccess.css'
+
 const SectionAccess = () => {
     return (
         <section className="access">
-            <h2>Access Clipboard anywhere</h2>
+            <h2 className="title">Access Clipboard anywhere</h2>
             <p className="description">Whether you’re on the go, or at your computer, you can access all your Clipboard
                 snippets in a few simple clicks.</p>
             <img src={imageDevices} alt="devices"></img>
