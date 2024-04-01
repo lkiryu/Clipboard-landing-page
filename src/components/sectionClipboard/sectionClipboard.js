adbd312be73385b533a3671e08a7be234da666c5
Kiryu <13matheusilva@gmail.com>
2024-04-01 17:36:12 -0300

    section clipboard style add

--- src/components/sectionClipboard/sectionClipboard.js
+++ src/components/sectionClipboard/sectionClipboard.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Buttons from "../buttons/buttons";
 
+import './sectionClipboard.css'
+
 const SectionClipboard = () => {
     return (
         <section className="clipboard">
-            <h2>Clipboard for iOS and Mac OS</h2>
+            <h2 className="title">Clipboard for iOS and Mac OS</h2>
             <p className="description">Available for free on the App Store. Download for Mac or iOS, sync with iCloud
                 and you’re ready to start adding to your clipboard.</p>
-                
+
             <Buttons />
         </section>
     )
