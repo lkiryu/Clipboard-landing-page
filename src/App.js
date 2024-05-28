a070369242cc18189a8581842c6d1ab4a712f1e8
kiryu <13matheusilva@gmail.com>
2024-05-28 20:06:51 -0300

    landing page changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import LandingPage from './components/landingPage/landingPage';
+import React from "react";
+import Header from "./components/header/header";
+import SectionKeepTrack from "./components/sectionKeepTrack/sectionKeepTrack";
+import SectionAccess from "./components/sectionAccess/sectionAccess";
+import SectionSupercharge from "./components/sectionSupercharge/sectionSupercharge";
+import SectionClipboard from "./components/sectionClipboard/sectionClipboard";
+import Footer from "./components/footer/footer";
 
 import './App.css';
 
 function App() {
   return (
-    <LandingPage/>
+    <div className="container">
+      <Header />
+
+      <SectionKeepTrack />
+
+      <SectionAccess />
+
+      <SectionSupercharge />
+
+      <SectionClipboard />
+
+      <Footer />
+    </div>
   );
 }
 
