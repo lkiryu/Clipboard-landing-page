5b666920d31b23e8539a13b88a8c6724c2e0c0ef
kiryu <13matheusilva@gmail.com>
2024-05-28 20:22:00 -0300

    section supercharge map add

--- src/components/sectionSupercharge/sectionSupercharge.js
+++ src/components/sectionSupercharge/sectionSupercharge.js
@@ -1,7 +1,6 @@
 import React from "react";
-import iconBlacklist from '../../images/icon-blacklist.svg'
-import iconText from '../../images/icon-text.svg'
-import iconPreview from '../../images/icon-preview.svg'
+import SuperchargeInfo from "../superchargeInfo/superchargeInfo";
+
 import iconGoogle from '../../images/logo-google.png'
 import iconIbm from '../../images/logo-ibm.png'
 import iconMicrosoft from '../../images/logo-microsoft.png'
@@ -10,6 +9,24 @@ import iconVectorGraphics from '../../images/logo-vector-graphics.png'
 
 import './sectionSupercharge.css'
 
+const infoData = [
+    {
+        src: "./images/icon-blacklist.svg",
+        title: "Create blacklists",
+        description: "Ensure sensitive information never makes its way to your clipboard by excluding certain sources.",
+    },
+    {
+        src: "./images/icon-preview.svg",
+        title: "Plain text snippets",
+        description: "Remove unwanted formatting from copied text for a consistent look.",
+    },
+    {
+        src: "./images/icon-text.svg",
+        title: "Sneak preview",
+        description: "Quick preview of all snippets on your Clipboard for easy access.",
+    },
+]
+
 const SectionSupercharge = () => {
     return (
         <section className="supercharge">
@@ -17,21 +34,18 @@ const SectionSupercharge = () => {
             <p className="description">We’ve got the tools to boost your productivity.</p>
             <div className="examples">
                 <ul>
-                    <li>
-                        <img src={iconBlacklist} alt="Blacklist"></img>
-                        <h3 className="title">Create blacklists</h3>
-                        <p className="description">Ensure sensitive information never makes its way to your clipboard by excluding certain sources.</p>
-                    </li>
-                    <li>
-                        <img src={iconText} alt="Blacklist"></img>
-                        <h3 className="title">Plain text snippets</h3>
-                        <p className="description">Remove unwanted formatting from copied text for a consistent look.</p>
-                    </li>
-                    <li>
-                        <img src={iconPreview} alt="Blacklist"></img>
-                        <h3 className="title">Sneak preview</h3>
-                        <p className="description">Quick preview of all snippets on your Clipboard for easy access.</p>
-                    </li>
+                    {
+                        infoData.map((item, index)=>{
+                            return(
+                                <SuperchargeInfo 
+                                    key={index}
+                                    src={item.src}
+                                    title={item.title}
+                                    description={item.description}
+                                />
+                            )
+                        })
+                    }
                 </ul>
                 <div className="brands">
                     <img src={iconGoogle} alt="icon google"></img>
